6928ef3b95c6866cc2e50ff246882ef58ef22878
SeoJungHong <hsj0223@gmail.com>
2017-11-12 15:33:23 +0900

    정리

---
+++ grad_project/constants.py
@@ -0,0 +1,17 @@
+DATA_PATH = 'data/'
+RAW_FILES = [
+    '1607.xlsx',
+    '1608.xlsx',
+    '1609.xlsx',
+    '1610.xlsx',
+    '1611.xlsx',
+    '1612.xlsx',
+    '1701.xlsx',
+    '1702.xlsx',
+    '1703.xlsx',
+    '1704.xlsx',
+    '1705.xlsx',
+    '1706.xlsx',
+    '1707.xlsx',
+    '1708.xlsx'
+]
--- grad_project/parse_excel_to_csv.py
+++ grad_project/parse_excel_to_csv.py
@@ -2,18 +2,11 @@ from functools import reduce
 
 import pandas as pd
 
-DATA_PATH = 'data/'
+from grad_project.constants import DATA_PATH, RAW_FILES
 
-file_header_map = {
-    '1607.xlsx': 5,
-    '1608.xlsx': 5,
-    '1609.xlsx': 5,
-    '1610.xlsx': 6,
-    '1611.xlsx': 6,
-    '1612.xlsx': 6,
-}
 sensor_column_nums = [0, 10, 8, 10, 8, 10, 10, 10, 8, 10, 8]
-for file in file_header_map.keys():
+
+for file in RAW_FILES:
     print('Reading ' + file)
     for i in range(1, 11):
         if i < 10:
@@ -29,7 +22,7 @@ for file in file_header_map.keys():
         print('read column index {0} ({1})'.format(column_index_list, len(column_index_list)))
         data = pd.read_excel(
             DATA_PATH + file,
-            header=file_header_map.get(file),
+            header=5 if file in ['1607.xlsx', '1608.xlsx', '1609.xlsx'] else 6,
             usecols=column_index_list,
             encoding='utf-8'
         )
@@ -39,55 +32,5 @@ for file in file_header_map.keys():
         data.to_csv(output_filename, index=False)
         print('save data to {0}'.format(output_filename))
         print('--------------------')
-
-usecols_map = {
-    1: [0, 1, 2, 6, 7, 9],
-    2: [0, 1, 2, 4, 5, 7],
-    3: [0, 1, 2, 6, 7, 9],
-    4: [0, 1, 2, 4, 5, 7],
-    5: [0, 1, 2, 6, 7, 9],
-    6: [0, 1, 2, 6, 7, 9],
-    7: [0, 1, 2, 6, 7, 9],
-    8: [0, 1, 2, 4, 5, 7],
-    9: [0, 1, 2, 6, 7, 9],
-    10: [0, 1, 2, 4, 5, 6],
-}
-
-for i in range(1, 11):
-    df_list = []
-    print('merge data from sensor {0}'.format(i))
-    for file in file_header_map.keys():
-        input_filename = '{0}{1}_{2}.csv'.format(DATA_PATH, file.split('.')[0], i)
-        print('read {0}'.format(input_filename))
-        df = pd.read_csv(input_filename, usecols=usecols_map.get(i))
-        df_list.append(df)
-    print('merge and sort values')
-    merged = pd.concat(df_list)
-    if i < 10:  # 기상 센서가 아닐때
-        prefix = 'N{0} '.format(i)
-        merged.rename(columns={
-            prefix + '날짜': '날짜',
-            prefix + '온도': '온도',
-            prefix + '습도': '습도',
-            prefix + '대기압': '대기압',
-            prefix + '조도': '조도',
-            prefix + 'CO2': 'CO2',
-        }, inplace=True)
-        # column 순서 정렬
-        columns = merged.columns.tolist()
-        columns.remove('CO2')
-        columns.append('CO2')
-        merged = merged[columns]
-    merged.sort_values(by=merged.columns[0], inplace=True)
-    print('remove empty rows')
-    merged[merged.columns[0]] = pd.DatetimeIndex(merged[merged.columns[0]])
-    merged["TMP"] = merged[merged.columns[0]]
-    merged = merged[merged.TMP.notnull()]  # remove all NaT values
-    merged.drop(["TMP"], axis=1, inplace=True)  # delete TMP again
-    merged.info()
-    output_filename = '{0}merged_{1}.csv'.format(DATA_PATH, i)
-    merged.to_csv(output_filename, index=False)
-    print('save merged data to {0}'.format(output_filename))
-    print('--------------------')
 
 print('Done')
