252afc9896de5a1d49ed09555999d8cd3226a9e8
SeoJungHong <hsj0223@gmail.com>
2017-11-12 15:03:09 +0900

    save to csv WIP

--- grad_project/parse_excel_to_csv.py
+++ grad_project/parse_excel_to_csv.py
@@ -40,18 +40,50 @@ for file in file_header_map.keys():
         print('save data to {0}'.format(output_filename))
         print('--------------------')
 
+usecols_map = {
+    1: [0, 1, 2, 6, 7, 9],
+    2: [0, 1, 2, 4, 5, 7],
+    3: [0, 1, 2, 6, 7, 9],
+    4: [0, 1, 2, 4, 5, 7],
+    5: [0, 1, 2, 6, 7, 9],
+    6: [0, 1, 2, 6, 7, 9],
+    7: [0, 1, 2, 6, 7, 9],
+    8: [0, 1, 2, 4, 5, 7],
+    9: [0, 1, 2, 6, 7, 9],
+    10: [0, 1, 2, 4, 5, 6],
+}
+
 for i in range(1, 11):
     df_list = []
     print('merge data from sensor {0}'.format(i))
     for file in file_header_map.keys():
         input_filename = '{0}{1}_{2}.csv'.format(DATA_PATH, file.split('.')[0], i)
         print('read {0}'.format(input_filename))
-        df = pd.read_csv(input_filename)
-        df.info()
+        df = pd.read_csv(input_filename, usecols=usecols_map.get(i))
         df_list.append(df)
-    print('* Merge and sort values')
+    print('merge and sort values')
     merged = pd.concat(df_list)
+    if i < 10:  # 기상 센서가 아닐때
+        prefix = 'N{0} '.format(i)
+        merged.rename(columns={
+            prefix + '날짜': '날짜',
+            prefix + '온도': '온도',
+            prefix + '습도': '습도',
+            prefix + '대기압': '대기압',
+            prefix + '조도': '조도',
+            prefix + 'CO2': 'CO2',
+        }, inplace=True)
+        # column 순서 정렬
+        columns = merged.columns.tolist()
+        columns.remove('CO2')
+        columns.append('CO2')
+        merged = merged[columns]
     merged.sort_values(by=merged.columns[0], inplace=True)
+    print('remove empty rows')
+    merged[merged.columns[0]] = pd.DatetimeIndex(merged[merged.columns[0]])
+    merged["TMP"] = merged[merged.columns[0]]
+    merged = merged[merged.TMP.notnull()]  # remove all NaT values
+    merged.drop(["TMP"], axis=1, inplace=True)  # delete TMP again
     merged.info()
     output_filename = '{0}merged_{1}.csv'.format(DATA_PATH, i)
     merged.to_csv(output_filename, index=False)
