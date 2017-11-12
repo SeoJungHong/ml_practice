4dbd655256680149902c14f1cf070d8ba048caa5
SeoJungHong <hsj0223@gmail.com>
2017-11-12 15:46:41 +0900

    remove empty rows

--- grad_project/merge_by_sensor.py
+++ grad_project/merge_by_sensor.py
@@ -39,7 +39,12 @@ for i in range(1, 11):
         columns.remove('CO2')
         columns.append('CO2')
         merged = merged[columns]
-    print(merged.head())
+    merged.info()
+    print('remove empty rows')
+    merged["TMP"] = merged[merged.columns[0]]
+    merged = merged[merged.TMP.notnull()]  # remove all NaT values
+    merged.drop(["TMP"], axis=1, inplace=True)  # delete TMP again
+    merged.info()
     output_filename = '{0}merged_{1}.csv'.format(DATA_PATH, i)
     merged.to_csv(output_filename, index=False)
     print('save merged data to {0}'.format(output_filename))
