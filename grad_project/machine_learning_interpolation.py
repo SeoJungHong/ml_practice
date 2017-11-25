60fff902d86388063fe598726278f01932963374
SeoJungHong <hsj0223@gmail.com>
2017-11-25 22:17:40 +0900

    add MLP

--- grad_project/machine_learning_interpolation.py
+++ grad_project/machine_learning_interpolation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 
 from grad_project.constants import OUTPUT_PATH, TEST_RATIO
 
@@ -19,29 +20,47 @@ raw_data = pd.read_csv('data/raw_data.csv', parse_dates=[0])
 raw_data.dropna(inplace=True)
 date = raw_data['date'].apply(lambda x: int(x.to_datetime().timestamp()))
 raw_data['date'] = date
-rmse = pd.DataFrame()
-r_square = pd.DataFrame()
-for column in raw_data.columns[1:]:
-    print('Start processing {0}'.format(column))
-    columns = FEATURE_MAP.get(column).append(column)
-    data = raw_data[FEATURE_MAP.get(column)]
-    print(data.head())
-    column_rmse = pd.DataFrame()
-    column_r_square = pd.DataFrame()
-    for index in range(1, 11):
-        print('run validation {0}'.format(index))
+
+for regressor_type in ['random_forest', 'mlp']:
+    print('##### Start {0} #####'.format(regressor_type))
+    rmse = pd.Series()
+    r_square = pd.Series()
+    for column in raw_data.columns[1:]:
+        print('Start processing {0}'.format(column))
+        columns = FEATURE_MAP.get(column).append(column)
+        data = raw_data[FEATURE_MAP.get(column)]
+        print(data.head())
         test_data = data.sample(frac=TEST_RATIO)
         train_data = data.drop(test_data.index)
-        regressor = RandomForestRegressor(random_state=0)
         train_y = train_data[column]
         train_x = train_data.drop(column, axis=1)
-        print('label')
+        print('train_y')
         print(train_y.head())
-        print('data')
+        print('train_x')
         print(train_x.head())
-        regressor.fit(train_x, train_y)
         test_y = pd.DataFrame(test_data[column])
         test_x = test_data.drop(column, axis=1)
+        if regressor_type == 'random_forest':
+            regressor = RandomForestRegressor(random_state=0)
+        else:
+            regressor = MLPRegressor(
+                learning_rate_init=0.001,
+                max_iter=500,
+                batch_size=5000,
+                early_stopping=True,
+                hidden_layer_sizes=(256, 256),
+                random_state=0, verbose=10
+            )
+        scaler = StandardScaler()
+        scaler.fit(train_x)
+        train_x = scaler.transform(train_x)
+        test_x = scaler.transform(test_x)
+        print('normalized data')
+        print('train_x')
+        print(train_x)
+        print('test_x')
+        print(test_x)
+        regressor.fit(train_x, train_y)
         raw_pred_y = regressor.predict(test_x)
         pred_y = pd.DataFrame({column: pd.Series(raw_pred_y, index=test_y.index)})
         print('##### Result #####')
@@ -49,15 +68,14 @@ for column in raw_data.columns[1:]:
         print(test_y.head())
         print('predicted Y')
         print(pred_y.head())
-        column_rmse[index] = ((test_y - pred_y) ** 2).mean(0) ** 0.5
-        column_r_square[index] = r2_score(test_y, pred_y, multioutput='raw_values')
-    column_rmse['mean'] = column_rmse.mean(1)
-    column_r_square['mean'] = column_r_square.mean(1)
-    print('column RMSE')
-    print(column_rmse.head())
-    print('column R-square')
-    print(column_r_square.head())
-    rmse = rmse.append(column_rmse)
-    r_square = r_square.append(column_r_square)
-    rmse.to_csv('{0}random_forest_result.csv'.format(OUTPUT_PATH))
-    r_square.to_csv('{0}random_forest_result_r2.csv'.format(OUTPUT_PATH))
+        rmse_value = (((test_y - pred_y) ** 2).mean(0) ** 0.5).values[0]
+        rmse.set_value(column, rmse_value)
+        r_square_value = regressor.score(test_x, test_y)
+        r_square.set_value(column, r_square_value)
+        print('RMSE')
+        print(rmse.head(10))
+        print('R-square')
+        print(r_square.head(10))
+        rmse.to_csv('{0}{1}_result.csv'.format(OUTPUT_PATH, regressor_type))
+        r_square.to_csv('{0}{1}_result_r2.csv'.format(OUTPUT_PATH, regressor_type))
+    print('##### {0} training Done #####'.format(regressor_type))
